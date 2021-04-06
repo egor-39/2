72892a66de51dfd6aa9c9b5e06a4eacb20204491
egor-39 <egor391@inbox.ru>
2021-04-06 16:29:24 +0200

    Добавьте файлы проекта.

---
+++ 2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string man = "cat";
+            //string girl = man + "lapa";
+
+            //int a = 15;
+            //int b = 8;
+            //    int c = a - b;
+            //int q = 15;
+            //int w = 8;
+            //int e = 7;
+            //int r = 5;
+            //    int t = (q + w) - e * r;
+            //double q = 7.5;
+            //int w = 2;
+            //int e = 8;
+            //double r = (q * w) + e;
+
+            //string a = "10";
+            //double b = double.Parse(a);
+            //string a = "10";
+            //int b = int.Parse(a);
+
+
+            //string a = "1";
+            //int a = 11;
+            //int b = 10;
+            //if (a<b)
+            //{
+            //    Console.WriteLine("a<b");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("a>b");
+
+            //}
+            //string a = "кот";
+            //string b = "ток";
+
+            //if (a==b)
+            //{
+
+            //    Console.WriteLine(a+" "+b);
+            //}
+            //b = a;
+
+            int d = 10;
+            int c = 3;
+            int v = d;
+            //результат: кот, Барсик!
+
+
+
+            Console.WriteLine(v);
+            Console.ReadKey();
+
+
+        }
+    }
+}
